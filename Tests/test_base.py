ddfb20409fbe56b40fcf441584b21fa9422cab14
Rafael Elias <rafael.elias@boomi.com>
2020-12-17 12:04:54 -0300

    Revert refactor in setup and teardown methods

--- Tests/test_base.py
+++ Tests/test_base.py
@@ -8,12 +8,10 @@ from Pages.SideBarPage import SideBarPage
 @pytest.mark.usefixtures("init_driver")
 class BaseTest:
 
-    def __init__(self):
-        self.sidebar = SideBarPage(self.driver)
-        self.loginPage = LoginPage(self.driver)
-
     def setup_method(self):
+        self.loginPage = LoginPage(self.driver)
         self.loginPage.do_login(TestData.STANDARD_USER_NAME, TestData.PASSWORD)
 
     def teardown_method(self):
+        self.sidebar = SideBarPage(self.driver)
         self.sidebar.do_logout()
